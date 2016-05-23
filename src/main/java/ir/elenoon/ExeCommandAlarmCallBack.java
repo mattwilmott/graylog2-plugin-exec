374116bb1a1bd9d993d761b3dcca37b569636a45
Matt Wilmott <matthew.wilmott@nttict.com.au>
2016-05-23 18:02:56 +1000

    updated to pass stream result and message via env vars. Needs further testing though, particularly message

--- src/main/java/ir/elenoon/ExeCommandAlarmCallBack.java
+++ src/main/java/ir/elenoon/ExeCommandAlarmCallBack.java
@@ -2,7 +2,11 @@ package ir.elenoon;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.graylog2.plugin.Message;
+import org.graylog2.plugin.alarms.AlertCondition;
 import org.graylog2.plugin.alarms.AlertCondition.CheckResult;
 import org.graylog2.plugin.alarms.callbacks.AlarmCallback;
 import org.graylog2.plugin.alarms.callbacks.AlarmCallbackConfigurationException;
@@ -22,10 +26,33 @@ public class ExeCommandAlarmCallBack implements AlarmCallback{
 	private Configuration configs;
 
 	@Override
-	public void call(Stream arg0, CheckResult arg1)
+	public void call(Stream stream, CheckResult result)
 			throws AlarmCallbackException {
 		try {
-			Runtime.getRuntime().exec(new String[]{"bash","-c",configs.getString("bashCommand")});
+			LinkedList<String> envp = new LinkedList<String>();
+			envp.add("streamId=" + stream.getId());
+			envp.add("streamTitle="+stream.getTitle());
+      			envp.add("streamDescription=" + stream.getDescription());
+        		envp.add("streamString=" + stream.toString());
+       			envp.add("AlarmDescription=" + result.getResultDescription());
+       			envp.add("AlarmString=" + result.toString());
+       			envp.add("AlarmTriggeredAt=" + result.getTriggeredAt().toString());
+       			envp.add("AlarmTriggeredConditionId=" + result.getTriggeredCondition().getId());
+       			envp.add("AlarmTriggeredConditionBacklog=" + result.getTriggeredCondition().getBacklog());
+       			envp.add("AlarmTriggeredConditionTypeString=" + result.getTriggeredCondition().getTypeString());
+       			envp.add("AlarmTriggeredConditionCreatedAt=" + result.getTriggeredCondition().getCreatedAt().toString());
+       			envp.add("AlarmTriggeredConditionDescription=" + result.getTriggeredCondition().getDescription());   
+			AlertCondition alertCondition = result.getTriggeredCondition();
+		        List<Message> messages = alertCondition.getSearchHits();
+       			Message message = null;
+            		if(0 < messages.size())
+            		{
+                		message = messages.get(0);
+				envp.add("MessageSource=" + message.getField("source"));   
+				envp.add("MessageFull=" + message.getField("full_message"));   
+            		}
+	
+			Runtime.getRuntime().exec(new String[]{"bash","-c",configs.getString("bashCommand")},envp.toArray(new String[envp.size()]));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
